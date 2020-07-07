97d0b44dc232158e8ca0e1c1de2aee140d9b0602
Parth Shastri <31370288+cppio@users.noreply.github.com>
2020-07-07 18:57:46 -0400

    Rewrite trampoline to have an api similar to swap

--- src/local/trampoline.rs
+++ src/local/trampoline.rs
@@ -9,75 +9,88 @@ use lde::X86;
 use lde::X64 as X86;
 
 pub struct Hook<T: 'static> {
-    detour_trampoline: T,
+    detour_target: T,
+    trampoline: isize,
 }
 
 impl<T> Hook<T> {
     pub const unsafe fn new(detour: T) -> Self {
         Self {
-            detour_trampoline: detour,
+            detour_target: detour,
+            trampoline: 0,
         }
     }
 }
 
 impl<T: Copy> Hook<T> {
     pub unsafe fn set_detour(&mut self, detour: T) {
-        self.detour_trampoline = detour;
+        self.detour_target = detour;
     }
 
-    pub unsafe fn hook(&mut self, target: T, trampoline: &'static mut [u8; 24]) {
-        let detour: isize = util::transmute(self.detour_trampoline);
+    pub unsafe fn set_trampoline(&mut self, trampoline: &'static mut [u8; 24]) {
+        trampoline[0] = 0xE9;
+        self.trampoline = trampoline.as_ptr() as isize - self as *mut _ as isize;
+    }
+
+    pub unsafe fn hook(&mut self, target: T) {
+        let detour: isize = util::transmute(self.detour_target);
         let target: isize = util::transmute(target);
 
-        let offset = i32::try_from(detour - target - 5).unwrap();
-        let offset2 = i32::try_from(target - trampoline.as_ptr() as isize - 5).unwrap();
+        let trampoline = &mut *((self.trampoline + self as *mut _ as isize) as *mut [u8; 24]);
 
-        let target = &mut *(target as *mut [u8; 19]);
+        let offset = i32::try_from(detour - target - 5).unwrap();
+        trampoline[1..5].copy_from_slice(&offset.to_ne_bytes());
+
+        let offset = i32::try_from(target - trampoline.as_ptr() as isize - 5).unwrap();
+
+        self.detour_target = util::transmute(target - self as *mut _ as isize);
+        let target = &*(target as *const [u8; 19]);
 
         let mut count = 0;
-
         while count < 5 {
             count += X86.ld(&target[count..]) as usize;
         }
 
-        trampoline[..count].copy_from_slice(&target[..count]);
-
-        target[0] = 0xE9;
-        target[1..5].copy_from_slice(&offset.to_ne_bytes());
+        trampoline[5..count].copy_from_slice(&target[5..count]);
 
         let jump = &mut trampoline[count..];
-
         jump[0] = 0xE9;
-        jump[1..5].copy_from_slice(&offset2.to_ne_bytes());
-        jump[5..].copy_from_slice(&[0xCC; 14][..19 - count]);
-
-        self.detour_trampoline =
-            util::transmute(trampoline as *mut _ as isize - self as *mut _ as isize);
+        jump[1..5].copy_from_slice(&offset.to_ne_bytes());
     }
 
     pub unsafe fn unhook(&mut self) {
-        let trampoline: isize = util::transmute(self.detour_trampoline);
-        let trampoline = (trampoline + self as *mut _ as isize) as *const [u8; 24];
+        let trampoline = &*((self.trampoline + self as *mut _ as isize) as *const [u8; 5]);
 
-        let jump = X86
-            .iter(&*trampoline, 0)
-            .fold(&[0; 5], |jump, (opcode, _)| {
-                (**opcode).try_into().unwrap_or(jump)
-            });
+        let offset = i32::from_ne_bytes(trampoline[1..].try_into().unwrap()) as isize;
 
-        let offset2 = i32::from_ne_bytes(jump[1..].try_into().unwrap()) as isize;
-        let target = (offset2 + trampoline as isize + 5) as *mut [u8; 5];
+        let target: isize = util::transmute(self.detour_target);
+        self.detour_target = util::transmute(offset + target + self as *mut _ as isize + 5);
+    }
 
-        let offset = i32::from_ne_bytes((*target)[1..].try_into().unwrap()) as isize;
-        self.detour_trampoline = util::transmute(offset + target as isize + 5);
+    #[inline(always)]
+    pub unsafe fn toggle_inline(&mut self) {
+        let trampoline = (self.trampoline + self as *mut _ as isize) as *mut [u8; 5];
 
-        *target = *(trampoline as *const _);
+        let target: isize = util::transmute(self.detour_target);
+        let target = (target + self as *mut _ as isize) as *mut _;
+
+        let scratch = *trampoline;
+        *trampoline = *target;
+        *target = scratch;
+    }
+
+    pub unsafe fn toggle(&mut self) {
+        self.toggle_inline()
+    }
+
+    pub unsafe fn target(&self) -> T {
+        let target: isize = util::transmute(self.detour_target);
+        util::transmute(target + self as *const _ as isize)
     }
 
     #[inline(always)]
     pub unsafe fn trampoline_inline(&self) -> T {
-        let trampoline: isize = util::transmute(self.detour_trampoline);
-        util::transmute(trampoline + self as *const _ as isize)
+        util::transmute(self.trampoline + self as *const _ as isize)
     }
 
     pub unsafe fn trampoline(&self) -> T {
@@ -100,10 +113,10 @@ macro_rules! local_trampoline_hook {
                 use super::super::*;
 
                 #[allow(unused_macros)]
-                macro_rules! unhook {
+                macro_rules! toggle {
                     () => {
                         #[allow(unused_unsafe)]
-                        unsafe { super::unhook() }
+                        unsafe { super::toggle() }
                     };
                 }
 
@@ -140,13 +153,28 @@ macro_rules! local_trampoline_hook {
                 $crate::local::trampoline::Hook::new(__ez_hook::$name)
             };
 
-            pub unsafe fn hook(target: __ez_Func, trampoline: &'static mut [u8; 24]) {
-                __ez_HOOK.hook(target, trampoline)
+            #[allow(dead_code)]
+            pub unsafe fn set_trampoline(trampoline: &'static mut [u8; 24]) {
+                __ez_HOOK.set_trampoline(trampoline)
+            }
+
+            pub unsafe fn hook(target: __ez_Func) {
+                __ez_HOOK.hook(target)
             }
 
             #[allow(dead_code)]
             pub unsafe fn unhook() {
                 __ez_HOOK.unhook()
+            }
+
+            #[allow(dead_code)]
+            pub unsafe fn toggle() {
+                __ez_HOOK.toggle()
+            }
+
+            #[allow(dead_code)]
+            pub unsafe fn target() -> __ez_Func {
+                __ez_HOOK.target()
             }
 
             #[allow(dead_code)]
@@ -179,10 +207,10 @@ mod tests {
         }
     }
 
-    fn setup() -> *mut [u8; 24] {
+    fn setup() -> &'static mut [u8; 24] {
         util::unprotect(square as _, 5);
 
-        util::allocate(square as _, 24).as_mut_ptr() as _
+        util::allocate(square as _, 24).try_into().unwrap()
     }
 
     #[test]
@@ -190,15 +218,28 @@ mod tests {
         let trampoline = setup();
 
         let mut hook = unsafe { Hook::<fn(i32) -> i32>::new(identity) };
+        unsafe { hook.set_trampoline(trampoline) };
 
         for _ in 0..2 {
-            unsafe { hook.hook(square, &mut *trampoline) };
+            unsafe { hook.hook(square) };
 
+            assert!(unsafe { hook.target() } == square);
+            assert_eq!(square(4), 16);
+            assert_eq!(square(5), 25);
+
+            unsafe { hook.toggle() };
+
+            assert!(unsafe { hook.target() } == square);
             assert_eq!(square(4), 4);
             assert_eq!(square(5), 5);
-
             assert_eq!(unsafe { hook.trampoline() }(4), 16);
             assert_eq!(unsafe { hook.trampoline() }(5), 25);
+
+            unsafe { hook.toggle() };
+
+            assert!(unsafe { hook.target() } == square);
+            assert_eq!(square(4), 16);
+            assert_eq!(square(5), 25);
 
             unsafe { hook.unhook() };
 
@@ -211,14 +252,28 @@ mod tests {
     fn hook_macro() {
         let trampoline = setup();
 
-        for _ in 0..2 {
-            unsafe { add_one_before::hook(square, &mut *trampoline) };
+        unsafe { add_one_before::set_trampoline(trampoline) };
 
+        for _ in 0..2 {
+            unsafe { add_one_before::hook(square) };
+
+            assert!(unsafe { add_one_before::target() } == square);
+            assert_eq!(square(4), 16);
+            assert_eq!(square(5), 25);
+
+            unsafe { add_one_before::toggle() };
+
+            assert!(unsafe { add_one_before::target() } == square);
             assert_eq!(square(4), 25);
             assert_eq!(square(5), 36);
-
             assert_eq!(unsafe { add_one_before::trampoline() }(4), 16);
             assert_eq!(unsafe { add_one_before::trampoline() }(5), 25);
+
+            unsafe { add_one_before::toggle() };
+
+            assert!(unsafe { add_one_before::target() } == square);
+            assert_eq!(square(4), 16);
+            assert_eq!(square(5), 25);
 
             unsafe { add_one_before::unhook() };
 
--- src/remote/trampoline.rs
+++ src/remote/trampoline.rs
@@ -1,11 +1,11 @@
 use crate::{local::trampoline::Hook, util};
 
-use core::{mem, slice};
+use core::{convert::TryInto, mem, slice};
 
 #[doc(hidden)]
 pub unsafe fn len<T: Copy>(end: &'static Hook<T>, start: T) -> usize {
     let start: usize = util::transmute(start);
-    end as *const _ as usize - start + mem::size_of_val(end)
+    end as *const _ as usize - start + mem::size_of_val(end) + 24
 }
 
 #[doc(hidden)]
@@ -14,11 +14,12 @@ pub unsafe fn copy_to<T: Copy>(
     start: T,
     dest: &'static mut [u8],
 ) -> &'static mut Hook<T> {
-    let size = len(end, start);
-    dest.copy_from_slice(slice::from_raw_parts(util::transmute(start), size));
+    let size = len(end, start) - 24;
+    dest[..size].copy_from_slice(slice::from_raw_parts(util::transmute(start), size));
 
     let remote = &mut *(dest[size - mem::size_of_val(end)..].as_mut_ptr() as *mut Hook<T>);
     remote.set_detour(util::transmute(dest.as_ptr()));
+    remote.set_trampoline((&mut dest[size..]).try_into().unwrap());
     remote
 }
 
@@ -38,6 +39,14 @@ macro_rules! remote_trampoline_hook {
             mod __ez_hook {
                 #[allow(unused_imports)]
                 use super::super::*;
+
+                #[allow(unused_macros)]
+                macro_rules! toggle {
+                    () => {
+                        #[allow(unused_unsafe)]
+                        unsafe { __ez_HOOK.toggle_inline() }
+                    };
+                }
 
                 #[allow(unused_macros)]
                 macro_rules! orig {
@@ -122,27 +131,38 @@ mod tests {
         static mut LAST: i32 = 0;
     }
 
-    fn setup(size: usize) -> (&'static mut [u8], *mut [u8; 24]) {
+    fn setup(size: usize) -> &'static mut [u8] {
         util::unprotect(square as _, 5);
 
-        let (dest, trampoline) = util::allocate(square as _, size + 24).split_at_mut(size);
-        (dest, trampoline.as_mut_ptr() as _)
+        util::allocate(square as _, size)
     }
 
     #[test]
     fn hook_macro() {
-        let (dest, trampoline) = setup(unsafe { add_one_before::len() });
+        let dest = setup(unsafe { add_one_before::len() });
 
         let hook = unsafe { add_one_before::copy_to(dest) };
 
         for _ in 0..2 {
-            unsafe { hook.hook(square, &mut *trampoline) };
+            unsafe { hook.hook(square) };
 
+            assert!(unsafe { hook.target() } == square);
+            assert_eq!(square(4), 16);
+            assert_eq!(square(5), 25);
+
+            unsafe { hook.toggle() };
+
+            assert!(unsafe { hook.target() } == square);
             assert_eq!(square(4), 25);
             assert_eq!(square(5), 36);
-
             assert_eq!(unsafe { hook.trampoline() }(4), 16);
             assert_eq!(unsafe { hook.trampoline() }(5), 25);
+
+            unsafe { hook.toggle() };
+
+            assert!(unsafe { hook.target() } == square);
+            assert_eq!(square(4), 16);
+            assert_eq!(square(5), 25);
 
             unsafe { hook.unhook() };
 
@@ -153,21 +173,22 @@ mod tests {
 
     #[test]
     fn hook_macro_state() {
-        let (dest, trampoline) = setup(unsafe { delayed::len() });
+        let dest = setup(unsafe { delayed::len() });
 
         let hook = unsafe { delayed::copy_to(dest) };
+        unsafe { hook.hook(square) };
 
-        unsafe { hook.hook(square, &mut *trampoline) };
+        unsafe { hook.toggle() };
 
         assert_eq!(square(4), 0);
         assert_eq!(square(5), 16);
 
-        unsafe { hook.unhook() };
+        unsafe { hook.toggle() };
 
         assert_eq!(square(4), 16);
         assert_eq!(square(5), 25);
 
-        unsafe { hook.hook(square, &mut *trampoline) };
+        unsafe { hook.toggle() };
 
         assert_eq!(square(4), 25);
         assert_eq!(square(5), 16);
